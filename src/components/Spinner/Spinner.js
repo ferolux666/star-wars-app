5b3987e1f6d6463abb4d0aa3eea169793b4ce3a0
Aleksey <aleksey.pakriev94@gmail.com>
2020-09-27 22:27:39 +0500

    Добавил прелоадер

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-12b9nu5fo3hr">
+            <div className="ldio-op42x4neg9">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
