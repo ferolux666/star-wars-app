8900ce41da31179a84086fc1c7459ff7dbee1211
Aleksey <aleksey.pakriev94@gmail.com>
2020-09-27 22:31:09 +0500

    Добавил логику индикатора загрузки и логику обработки ошибок.

--- src/components/Random-planet/Random-planet.js
+++ src/components/Random-planet/Random-planet.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import './Random-planet.css';
 import SwapiService from "../../services/swapi-service";
+import Spinner from "../Spinner/Spinner";
+import ErrorIndicator from "../Error-indicator/Error-indicator";
 
 export default class RandomPlanet extends Component {
 
     state = {
-        planet: {}
+        planet: {},
+        loading: true,
+        error: false,
     }
 
     constructor(props) {
@@ -15,42 +19,72 @@ export default class RandomPlanet extends Component {
 
     swApi = new SwapiService();
 
+    onPlanetLoaded = (planet) => {
+        this.setState({
+            planet,
+            loading: false
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
     updatePlanet = () => {
-        const id = Math.floor(Math.random() * 11 + 2);
+        const id = 12;
         this.swApi.getPlanet(id)
-            .then(planet => {
-                this.setState({
-                    planet
-                })
-            })
+            .then(this.onPlanetLoaded)
+            .catch(this.onError)
     }
 
     render() {
 
-        const { planet: { id, name, population, rotationPeriod, diameter }  } = this.state;
+        const {planet, loading, error} = this.state;
+
+        const hasData = !(error || loading);
+        const errorIndicator = error ? <ErrorIndicator/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = hasData ? <ViewPlanet planet={planet}/> : null;
+
         return (
             <div className="random-planet jumbotron rounded">
-                <img className="planet-image"
-                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-                     alt="Картинка рандомной планеты"/>
-                <div>
-                    <h4>{name}</h4>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span className="term">Population</span>
-                            <span>{population}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Rotation Period</span>
-                            <span>{rotationPeriod}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Diameter</span>
-                            <span>{diameter}</span>
-                        </li>
-                    </ul>
-                </div>
+                {spinner}
+                {content}
+                {errorIndicator}
             </div>
         );
     }
+}
+
+const ViewPlanet = ({planet}) => {
+
+    const { id, name, population, rotationPeriod, diameter } = planet;
+
+    return (
+        <React.Fragment>
+            <img className="planet-image"
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+                 alt="Картинка рандомной планеты"/>
+            <div>
+                <h4>{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{population}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{rotationPeriod}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{diameter}</span>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
+    )
 }
