ce81607d7ca82de491290515b5520c83a7ef87d2
Aleksey <aleksey.pakriev94@gmail.com>
2020-09-28 21:15:39 +0500

    Подправил ошибку в возращаемом объекте

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -74,8 +74,10 @@ export default class SwapiService {
             id: this._extractId(person),
             name: person.name,
             gender: person.gender,
-            birthYear: person.birthYear,
-            eyeColor: person.eyeColor
+            birthYear: person["birth_year"],
+            eyeColor: person["eye_color"],
+            height: person.height,
+            mass: person.mass
         }
     }
 }
