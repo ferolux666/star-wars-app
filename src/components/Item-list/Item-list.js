1f8d6705fff9840370f65937d78d4e5bf8dcbf70
Aleksey <aleksey.pakriev94@gmail.com>
2020-09-28 21:17:15 +0500

    Добавил state и методы жизненого цикла

--- src/components/Item-list/Item-list.js
+++ src/components/Item-list/Item-list.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './Item-list.css';
+import SwapiService from "../../services/swapi-service";
+import Spinner from "../Spinner/Spinner";
+import ErrorButton from "../Error-button/Error-button";
 
 export default class ItemList extends Component {
 
+    swapi = new SwapiService();
+
+    state = {
+        itemList: null
+    }
+
+    componentDidMount = () => {
+        this.swapi.getAllPeople()
+            .then(itemList => {
+                this.setState({
+                    itemList
+                })
+            })
+    }
+
+    renderItems = (items) => {
+        return items.map(({id, name}) => {
+            return (
+                <li className="list-group-item"
+                    key={id}
+                    onClick={() => this.props.onItemSelected(id)}>
+                    {name}
+                </li>
+            )
+        })
+    }
+
+
     render() {
+
+        const { itemList } = this.state;
+
+        if(!itemList) {
+            return (
+                <Spinner/>
+            )
+        }
+
+        const items = this.renderItems(itemList);
+
         return (
             <ul className="item-list list-group">
-                <li className="list-group-item">
-                    Luke Skywalker
-                </li>
-                <li className="list-group-item">
-                    Darth Vader
-                </li>
-                <li className="list-group-item">
-                    R2-D2
-                </li>
+                {items}
             </ul>
         );
     }
--- src/components/Person-details/Person-details.js
+++ src/components/Person-details/Person-details.js
@@ -1,32 +1,88 @@
 import React, { Component } from 'react';
-
 import './Person-details.css';
+import SwapiService from "../../services/swapi-service";
+import ErrorButton from "../Error-button/Error-button";
 
 export default class PersonDetails extends Component {
 
+    swapi = new SwapiService();
+
+    state = {
+        itemDetails: null,
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount")
+        this.updateItemInfo();
+    }
+
+    componentDidUpdate(prevProps) {
+        console.log("componentDidUpdate")
+        if(prevProps.itemSelected !== this.props.itemSelected) {
+            this.updateItemInfo();
+        }
+    }
+
+    onLoaded = (id) => {
+        console.log("onloaded")
+        this.swapi.getPerson(id)
+            .then(item => {
+                this.setState({
+                    itemDetails: item,
+                })
+            })
+    }
+
+    updateItemInfo = () => {
+        console.log("update")
+        const id = this.props.itemSelected;
+        if(!id) {
+            return;
+        }
+        this.onLoaded(id);
+    }
+
     render() {
+        console.log("render")
+
+        const { itemDetails } = this.state
+
+        if (!itemDetails) {
+            return <span>Выберите персоажа</span>
+        }
+
         return (
             <div className="person-details card">
-                <img className="person-image"
-                     src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
-                <div className="card-body">
-                    <h4>R2-D2</h4>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span className="term">Gender</span>
-                            <span>male</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Birth Year</span>
-                            <span>43</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Eye Color</span>
-                            <span>red</span>
-                        </li>
-                    </ul>
-                </div>
+                <ViewItem itemDetails={itemDetails}/>
             </div>
         )
     }
+}
+
+const ViewItem = ({itemDetails}) => {
+    const { id, name, gender, birthYear, eyeColor } = itemDetails;
+    return (
+        <React.Fragment>
+            <img className="person-image"
+                 src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} alt={name}/>
+            <div className="card-body">
+                <h4>{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Gender</span>
+                        <span>{gender}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Birth Year</span>
+                        <span>{birthYear}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Eye Color</span>
+                        <span>{eyeColor}</span>
+                    </li>
+                </ul>
+                <ErrorButton/>
+            </div>
+        </React.Fragment>
+    )
 }
