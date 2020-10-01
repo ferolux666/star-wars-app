ce81ead3bfb2f6db1357564a745c44873d42630f
Aleksey <aleksey.pakriev94@gmail.com>
2020-10-01 11:54:16 +0500

    Добавил отрисовку компонентов на основе полученых данных

--- src/components/Item-details/Item-details.js
+++ src/components/Item-details/Item-details.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import './Person-details.css';
+import './Item-details.css';
 import SwapiService from "../../services/swapi-service";
 import Spinner from "../Spinner/Spinner";
 import ErrorButton from "../Error-button/Error-button";
 
-export default class PersonDetails extends Component {
+export const Record = ({label, field, itemDetails}) => {
+    return (
+        <li className="list-group-item">
+            <span className="term">{label}:</span>
+            <span>{itemDetails[field]}</span>
+        </li>
+    )
+}
+
+export default class ItemDetails extends Component {
 
     swapi = new SwapiService();
 
     state = {
         itemDetails: null,
+        imageUrl: null
     }
 
     componentDidMount() {
@@ -22,25 +32,26 @@ export default class PersonDetails extends Component {
         }
     }
 
-    onLoaded = (id) => {
-        this.swapi.getPerson(id)
+    onLoaded = (id, imageUrl) => {
+        this.props.getData(id)
             .then(item => {
                 this.setState({
                     itemDetails: item,
+                    imageUrl: imageUrl(id),
                 })
             })
     }
 
     updateItemInfo = () => {
-        const id = this.props.itemSelected;
-        if(!id) {
+        const { itemId, imageUrl } = this.props
+        if(!itemId) {
             return;
         }
-        this.onLoaded(id);
+        this.onLoaded(itemId, imageUrl);
     }
 
     render() {
-        const { itemDetails } = this.state
+        const { itemDetails, imageUrl } = this.state
 
         if (!itemDetails) {
             return <Spinner/>
@@ -48,33 +59,25 @@ export default class PersonDetails extends Component {
 
         return (
             <div className="person-details card">
-                <ViewItem itemDetails={itemDetails}/>
+                <ViewItem itemDetails={itemDetails} imageUrl={imageUrl} children={this.props.children}/>
             </div>
         )
     }
 }
 
-const ViewItem = ({itemDetails}) => {
-    const { id, name, gender, birthYear, eyeColor } = itemDetails;
+const ViewItem = ({itemDetails, imageUrl, children}) => {
+    debugger;
+    const { name } = itemDetails;
     return (
         <React.Fragment>
             <img className="person-image"
-                 src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} alt={name}/>
+                 src={imageUrl} alt={name}/>
             <div className="card-body">
                 <h4>{name}</h4>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item">
-                        <span className="term">Gender</span>
-                        <span>{gender}</span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="term">Birth Year</span>
-                        <span>{birthYear}</span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="term">Eye Color</span>
-                        <span>{eyeColor}</span>
-                    </li>
+                    {React.Children.map(children, child => {
+                        return React.cloneElement(child, {itemDetails})
+                    })}
                 </ul>
                 <ErrorButton/>
             </div>
